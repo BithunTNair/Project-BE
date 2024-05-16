6ea537150fae9348c324223dd8c3f8a51f4f3fbf
BithunT-Nair <react666cr@gmail.com>
2024-05-17 01:51:54 +0530

    small change

--- middlewares/authorization.js
+++ middlewares/authorization.js
@@ -22,11 +22,12 @@ const userAuth = (req, res, next) => {
 
 const adminAuth = (req, res, next) => {
     try {
-        console.log(req.headers);
+       
         const token = req.headers['authorization'].split(' ')[1]
         jwt.verify(token, process.env.JWT_PASSWORD, (err, decodedToken) => {
             if (decodedToken && decodedToken._doc.role === 1) {
                 req.userId = decodedToken._doc._id
+                console.log('***');
                 next()
             } else{
                 res.status(401).json({ message: 'unauthorized user' })
