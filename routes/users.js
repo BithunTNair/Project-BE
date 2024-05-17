dee8d233d30396306164d83d99c210587993ccc1
BithunT-Nair <react666cr@gmail.com>
2024-05-17 16:57:49 +0530

    thursdaybe commit

--- routes/users.js
+++ routes/users.js
@@ -1,9 +1,9 @@
 var express = require('express');
 var router = express.Router();
+const {userAuth} = require('../middlewares/authorization');
+const {getAllCourtData,singleCourtData}=require('../controllers/userController')
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.get('/getallcourtdata',userAuth,getAllCourtData );
+router.get('/getsinglecourtdata',userAuth,singleCourtData );
 
 module.exports = router;
